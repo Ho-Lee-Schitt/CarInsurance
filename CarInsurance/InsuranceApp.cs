1bf7265a0b6defe72ea58cf35d32d46b955ed775
Unknown <methos1014@gmail.com>
2017-10-15 03:07:41 +0100

    Formatting and layout improvements.
    Formatted the creation page to better structure the form data.

--- CarInsurance/InsuranceApp.cs
+++ CarInsurance/InsuranceApp.cs
@@ -15,7 +15,36 @@ namespace CarInsurance
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime PolStartDate { get; set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<DriverDetails> DriverList { get; set; }
+        public struct Driver
+        {
+            [Required(ErrorMessage = "Driver Name is a required field")]
+            [Display(Name = "Driver Name")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Occupation is a required field")]
+            [Display(Name = "Occupation")]
+            public string Occupation { get; set; }
+
+            [Required(ErrorMessage = "Date Of Birth is a required field")]
+            [Display(Name = "Date Of Birth")]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            public DateTime DOB { get; set; }
+
+            public class DateTimeWrapper
+            {
+                [Display(Name = "")]
+                [DataType(DataType.Date)]
+                [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+                public DateTime Value { get; set; }
+            }
+
+            [Display(Name = "Claim Date")]
+            public List<DateTimeWrapper> Claims { get; set; }
+        }
+
+        [Required(ErrorMessage = "Driver is a required field")]
+        [Display(Name = "Driver")]
+        public List<Driver> DriverDetails { get; set; }
     }
 }
